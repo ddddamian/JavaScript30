ddbcbb9a23892802b8aa1ab452b3019c5743e21a
Pete <peterwalsh86@hotmail.com>
2018-04-09 13:43:59 +0100

    Update script.js

--- 29-Countdown Timer/script.js
+++ 29-Countdown Timer/script.js
@@ -1,11 +1,13 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
     console.log({now, then});
     displayTimeLeft(seconds); // run immediately to avoid a 1 second delay on start
+    displayEndTime(then);
 
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -23,6 +25,14 @@ function displayTimeLeft(seconds) {
     const minutes = Math.floor(seconds / 60);
     const remainderSeconds = seconds % 60;
     const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    document.title = display;
     timerDisplay.textContent = display;
     console.log({minutes, remainderSeconds});
+}
+
+function displayEndTime (timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${hour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
