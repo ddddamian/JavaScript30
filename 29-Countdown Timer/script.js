d45f1ff27530d749ebd585f055a1a219a82700d5
Pete <peterwalsh86@hotmail.com>
2018-04-11 12:36:51 +0100

    Update script.js

--- 29-Countdown Timer/script.js
+++ 29-Countdown Timer/script.js
@@ -1,8 +1,14 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]')
+
+
 
 function timer(seconds) {
+    // clear any existing timers
+    clearInterval(countdown);
+
     const now = Date.now();
     const then = now + seconds * 1000;
     console.log({now, then});
@@ -36,3 +42,18 @@ function displayEndTime (timestamp) {
     const minutes = end.getMinutes();
     endTime.textContent = `Be Back At ${hour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+    timer(mins * 60);
+    this.reset();
+});
